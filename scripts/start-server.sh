550d8008ad22584494e7166ccc9893330d097406
Juwoojae <juwqq1234@naver.com>
2025-11-12 19:49:37 +0900

    제발 되라 제발

---
+++ scripts/start-server.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "--------------서버 시작-----------------"
+
+cd /home/ubuntu/instagram-server
+sudo fuser -k -n tcp 8080 || true
+nohub java -jar project.jar > ./output.log 2>&1 &
+echo "--------------서버 배포 끝--------------------"
